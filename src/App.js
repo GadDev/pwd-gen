3b4637dc11ddfdae6f15925a6f7180d7540bbaa3
Alex Gadaix <alex.gadaix@gmail.com>
2021-04-10 21:21:16 +0100

    scaffold basic html structure and styling

--- src/App.js
+++ src/App.js
@@ -5,23 +5,40 @@ import { useState } from "react";
 import { genPwd } from "./utils/genPwd";
 function App() {
   const [value, setValue] = useState("");
-  // const [length, setLength] = useState;
+  const [pwdStrength, setPwdStrength] = useState("16");
 
   const handleClick = () => {
-    let pwd = genPwd(17);
+    let pwd = genPwd(pwdStrength);
     setValue(pwd);
   };
 
   return (
     <div className="App">
-      <h1>MP-Password generator</h1>
-      <div>
-        <label htmlFor="password">
-          <input type="text" value={value} placeholder="Create Password" />
-        </label>
-      </div>
-      <button onClick={() => handleClick()}>Generate Password</button>
-      <button onClick={() => setValue("")}>Reset</button>
+      <header>
+        <h1>MP-Password generator</h1>
+      </header>
+      <main>
+        <section>
+          <div>
+            <label htmlFor="pwd-strength">
+              Set the password length
+              <input
+                id="pwd-strength"
+                type="number"
+                value={pwdStrength}
+                onChange={(e) => setPwdStrength(e.target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="password">
+              <input type="text" value={value} placeholder="Create Password" />
+            </label>
+          </div>
+          <button onClick={() => handleClick()}>Generate Password</button>
+          <button onClick={() => setValue("")}>Reset</button>
+        </section>
+      </main>
     </div>
   );
 }
