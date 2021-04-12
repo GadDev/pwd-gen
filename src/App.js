f5453dc89fb8be46947d63e57f5b99bed51d3dce
Alex Gadaix <alex.gadaix@gmail.com>
2021-04-12 10:21:24 +0100

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>M-Password generator</h1>
+        <h1>MP-Password generator</h1>
       </header>
       <main>
         <section>
